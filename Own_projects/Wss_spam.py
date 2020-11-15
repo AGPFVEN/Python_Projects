9bc7922f086401bc6c308e46713e00d60c05b873
AGPFVEN <50179663+AGPFVEN@users.noreply.github.com>
2020-11-15 03:01:08 +0100

    try some py setup

--- Own_projects/Wss_spam.py
+++ Own_projects/Wss_spam.py
@@ -1,13 +1,12 @@
-# import webbrowser 
+import sys
+sys.path.append("D:\\python_agpfven\\lib\\site-packages")
 
-# webbrowser.open_new("https://web.whatsapp.com/")
 from selenium import webdriver
 
 path = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
 driver = webdriver.Chrome(path)
+driver.get("https://www.wikipedia.org/")
 
-driver.get("https://web.whatsapp.com")
-
-# contact_search = driver.find_element_by_class_name( "_3FRCZ copyable-text selectable-text")
+# contact_search = driver.find_exelement_by_class_name( "_3FRCZ copyable-text selectable-text")
 
 # print(contact_search)
