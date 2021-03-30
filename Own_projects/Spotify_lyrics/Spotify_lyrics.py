9442741caecf1438dbb01475b705d47c1e6a3867
AGPFVEN <50179663+AGPFVEN@users.noreply.github.com>
2021-03-30 17:30:51 +0200

    New little project

---
+++ Own_projects/Spotify_lyrics/Spotify_lyrics.py
@@ -0,0 +1,21 @@
+import os
+import sys
+import json
+import spotipy
+import webbrowser
+import spotipy.util as util
+from json.decoder import JSONDecodeError
+
+#Variables that I will need
+username = sys.argv[1]
+scope = 'user-read-private user-read-playback-state user-modify-playback-state'
+
+try:
+    token = util.prompt_for_user_token(username, scope)
+except(AttributeError, JSONDecodeError):
+    os.remove(f".cache-{username}")
+    token = util.prompt_for_user_token(username, scope)
+
+#connect to spotify
+def connectspotify:
+    #
