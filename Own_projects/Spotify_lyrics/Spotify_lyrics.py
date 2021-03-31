a97580b2ec72b02f9f47995593da650f85e9efa1
AGPFVEN <50179663+AGPFVEN@users.noreply.github.com>
2021-03-31 23:45:10 +0200

    input username

--- Own_projects/Spotify_lyrics/Spotify_lyrics.py
+++ Own_projects/Spotify_lyrics/Spotify_lyrics.py
@@ -7,7 +7,7 @@ import spotipy.util as util
 from json.decoder import JSONDecodeError
 
 #Variables that I will need
-username = sys.argv[1]
+username = input()
 scope = 'user-read-private user-read-playback-state user-modify-playback-state'
 
 try:
@@ -15,7 +15,3 @@ try:
 except(AttributeError, JSONDecodeError):
     os.remove(f".cache-{username}")
     token = util.prompt_for_user_token(username, scope)
-
-#connect to spotify
-def connectspotify:
-    #
