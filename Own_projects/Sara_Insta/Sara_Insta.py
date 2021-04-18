20ed1ca78577521a2839f321bac7be2c7ed26831
AGPFVEN <50179663+AGPFVEN@users.noreply.github.com>
2021-04-18 06:16:50 +0200

    new miniproject

---
+++ Own_projects/Sara_Insta/Sara_Insta.py
@@ -0,0 +1,13 @@
+from requests import get
+from bs4 import BeautifulSoup
+
+usr_agent = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                    'Chrome/61.0.3163.100 Safari/537.36'}
+
+insta_url = 'https://www.instagram.com/saraabasolo/'
+response = get(insta_url, headers=usr_agent)
+response.raise_for_status()
+soup = BeautifulSoup(response.text, "html.parser")
+result_block = soup.find_all('div', {'class': 'v1Nh3 kIKUG  _bz0w'})
+print(soup)
