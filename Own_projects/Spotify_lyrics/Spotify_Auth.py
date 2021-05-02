642b501d9e6b32a595844ef3f1bcb60424086f22
AGPFVEN <50179663+AGPFVEN@users.noreply.github.com>
2021-05-02 02:46:02 +0200

    auth process

--- Own_projects/Spotify_lyrics/Spotify_Auth.py
+++ Own_projects/Spotify_lyrics/Spotify_Auth.py
@@ -1,6 +1,6 @@
-import spotipy
-from spotipy.oauth2 import SpotifyOAuth
 import Spotify_Credentials
+from spotipy.oauth2 import SpotifyOAuth
+from requests.auth import HTTPBasicAuth
 from flask import Flask, request, url_for, session, redirect
 
 app = Flask(__name__)
@@ -10,7 +10,13 @@ app.config['SESSION_COOKIE_NAME'] = 'Jasons Cookie'
 
 @app.route('/')
 def index():
-    return 'Jasons home page'
+    sp_oauth = create_spotify_Oauth()
+    auth_url = sp_oauth.get
+    return redirect(auth_url)
+
+@app.route('/redirect')
+def redirect():
+    return 'redirect'
 
 @app.route('/getTracks')
 def getTracks():
@@ -18,11 +24,12 @@ def getTracks():
 
 # Request token
 # request = requests.get(url_token, )
+
 def create_spotify_Oauth():
     return spotifyOAuth(
         client_id = Spotify_Credentials.client_id,
         client_secret = Spotify_Credentials.client_secret,
-        redirect_uri = url_for('localhost:5000/redirect', _external = True),
+        redirect_uri = url_for('/redirect', _external = True),
         scope = "user-library-read"
     )
 
@@ -30,3 +37,6 @@ def create_spotify_Oauth():
 url_token = 'https://accounts.spotify.com/authorize'
 client_id = Spotify_Credentials.client_id
 response_type = 'code'
+
+if __name__ == "__main__":
+    app.run(debug=True)
