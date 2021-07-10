067e16293f275bdf0489b581dbadd25a3a892242
AGPFVEN <50179663+AGPFVEN@users.noreply.github.com>
2021-07-11 00:07:14 +0200

    finding paths

--- Own_projects/File_Helper/File_Helper.py
+++ Own_projects/File_Helper/File_Helper.py
@@ -1,7 +1,39 @@
-import os
-import shutil
+import os,shutil
+from pathlib import Path
 
-print("Select a directory (carpet)")
-directory_from = input()
+def Find_folder():
+    path = r'C:\Users\alfon\Downloads'
+    # first_files = os.listdir(path)
+    first_files = sorted(Path(path).iterdir(), key=os.path.getmtime)
+    print(first_files)
+    print("is this your folder? y/n")
+    fist_file = first_files[0]
+    response_input = input()
+    
+    if(response_input == "y"):
+        return fist_file
+    
+    elif(response_input == "n"):
+        print("no")
 
+    else:
+        print("invalid response try it again:")
+        Find_folder()
+
+def Find_folder__(my_folder):
+    path = r'C:\Users\alfon\Downloads'
+    first_files = os.listdir(path)
+    
+    for elements in first_files:
+        if(my_folder == "elements"):
+            return elements
+        
+        print("not found")
+
+
+print(Find_folder())
+
+
+
+#Descargas
 #BEYONDTECH EUROPE/CONTABILIDAD/Facturas EMITIDAS- Clientes -PAN EUROPEO/Pan Europa/Por Paises/España
