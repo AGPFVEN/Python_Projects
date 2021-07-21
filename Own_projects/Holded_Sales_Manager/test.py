03fe4524cea9a7ae09e07fa5b50753c1edeee1c8
AGPFVEN <50179663+AGPFVEN@users.noreply.github.com>
2021-07-21 06:48:45 +0200

    update

--- Own_projects/Holded_Sales_Manager/test.py
+++ Own_projects/Holded_Sales_Manager/test.py
@@ -1,3 +1,4 @@
+from pdfminer import high_level
 import Secret_Holder
 import requests
 
@@ -9,6 +10,18 @@ def Get_List_Of_Docs():
     headers = {
         "Accept": "application/json",
         "key": Secret_Holder.Holded_API
+    }
+
+    response = requests.request("GET", url, headers=headers)
+
+    return response
+
+def Get_Doc_by_DocId(docId):
+    url = "https://api.holded.com/api/invoicing/v1/documents/salesreceipt/" + docId
+
+    headers = {
+        "Accept": "application/json",
+        "key": "0476c2dcf451e748daf1c33c08c746cf"
     }
 
     response = requests.request("GET", url, headers=headers)
@@ -28,8 +41,34 @@ def Group_DocId_By_DocNum():
             if not (("\"paymentsDetail\":[{\"id\"" in element) or ("\"from\":{\"id\":\"" in element)):
                 yield element
 
+def Extract_Doc_text(filename): #This function can fail be carefull
+    text = high_level.extract_text(filename)
+    text_separated_by_new_line = text.split("\n")
+    return text_separated_by_new_line
 
-# print(Get_List_Of_Docs())
-mylist = Group_DocId_By_DocNum()
-for i in mylist:
-    print(i)
+def Extract_DocNum_of_pdf(doc_text):
+    for elements in doc_text:
+        new_elements = elements.split("-")
+        if 3 == len(new_elements): 
+            if (str.isdecimal(new_elements[0])and str.isdecimal(new_elements[1]) and str.isdecimal(new_elements[2])):
+                return elements
+    
+# INV-ES-910864895-2021-184.pdf
+
+doc_text = Extract_Doc_text("INV-ES-910864895-2021-184.pdf")
+docNum = Extract_DocNum_of_pdf(doc_text)
+list_Of_Docs = Group_DocId_By_DocNum()
+for i in list_Of_Docs:
+    if ("id" in i):
+        new_docId = i
+
+    if ("desc" in i):
+        new_docNum = i 
+
+        if(docNum in new_docNum):
+            break
+
+docIds = new_docId.split("\"")
+docId = docIds[3]
+Doc = Get_Doc_by_DocId(docId)
+print(doc_text)
