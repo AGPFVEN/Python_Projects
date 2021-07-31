aae0327c717f9a7c1efcbb2c21b4dec2d5b2b7f8
AGPFVEN <50179663+AGPFVEN@users.noreply.github.com>
2021-07-31 16:23:30 +0200

    a lot of work

--- Own_projects/Holded_Sales_Manager/test.py
+++ Own_projects/Holded_Sales_Manager/test.py
@@ -1,5 +1,7 @@
-import requests
+from os import replace
 from pdfminer import high_level
+from pdf2image import convert_from_path
+import requests
 import Secret_Holder
 
 # https://api.holded.com/api/invoicing/v1/documents/docType/documentId/pdf Get_List_Of_DocId_With_DocNum
@@ -55,19 +57,49 @@ def Extract_DocNum_of_pdf(doc_text):
             if (str.isdecimal(new_elements[0])and str.isdecimal(new_elements[1]) and str.isdecimal(new_elements[2])):
                 return elements
 
-def Extract_info_of_pdf(doc_text):
-    id_ = 0
-    for elements in doc_text:
-        if("Karsten Otto" in elements):
-            return id_, elements
+def Extract_info_of_pdf(pdf_text, doc_name_):
+    i = 0
 
-        id_ += 1
+    for element in pdf_text:
+
+        if doc_name_ == element:
+            print(doc_name_)
+            print(element)
+            k = pdf_text.index(element)
+            fecha = pdf_text[k - 2]
+
+        if "€" in element:
+            i = pdf_text.index(element)
+
+        if "www.amazon." in element:
+            nombre = pdf_text[i + 1]
             
+            if ((pdf_text[i + 18].replace(" ", "") in pdf_text[i + 19]) and (pdf_text[i + 18].replace(" ", "") != pdf_text[i + 19].replace(" ", ""))):
+                cif = pdf_text[i + 19]
+                return nombre, cif, fecha
 
-    
-# INV-ES-910864895-2021-184.pdf INV-DE-910864895-2021-40.pdf
+            return nombre, fecha
 
-doc_text = Extract_Doc_text("CN-DE-910864895-2021-3.pdf")
+
+def Generator_to_array(generator):
+    array = []
+
+    for i in generator:
+        array.append(i)
+
+    return array
+
+# INV-DE-910864895-2021-40.pdf CN-DE-910864895-2021-3.pdf CN-FR-910864895-2021-3.pdf INV-FR-910864895-2021-65.pdf
+
+#Extract_info_of_pdf('INV-ES-910864895-2021-184.pdf')
+
+
+# doc_name = "INV-DE-910864895-2021-40.pdf"
+# doc_name = "CN-DE-910864895-2021-3.pdf"
+# doc_name = "CN-FR-910864895-2021-3.pdf"
+doc_name = "INV-FR-910864895-2021-65.pdf"
+
+doc_text = Extract_Doc_text(doc_name)
 docNum = Extract_DocNum_of_pdf(doc_text)
 list_Of_Docs = Group_DocId_By_DocNum()
 
@@ -84,5 +116,17 @@ for i in list_Of_Docs:
 docIds = new_docId.split("\"")
 docId = docIds[3]
 Doc = Get_Doc_by_DocId(docId)
-doc_info = Extract_info_of_pdf(doc_text)
+
+
+doc_text_ = Extract_Doc_text(doc_name)
+array_text = Generator_to_array(doc_text_)
+doc_info = Extract_info_of_pdf(array_text, doc_name.replace('.pdf', ""))
+print(array_text)
 print(doc_info)
+# print(array_text.index(doc_info))
+
+# for i in doc_text:
+#     print(i)
+# kk =Generator_to_array(Extract_Doc_text('INV-DE-910864895-2021-40.pdf'))
+# print(kk[1])
+# print(high_level.extract_text('INV-DE-910864895-2021-40.pdf'))
